60768189072f246dd628d5d41800776c12c86b3f
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-09-11 19:25:07 -0300

    Reduce shopping token usage to 586

--- shopping.lua
+++ shopping.lua
@@ -11,7 +11,7 @@ basket = {}
 
 function shopping_open()
     local shopping_cb = function()
-        shopping_routine_drawing(
+        shopping_routine_draw(
                 {
                     {
                         id = "potion",
@@ -38,37 +38,31 @@ function shopping_open()
     )
 end
 
+function shopping_routine_update(options)
 
-function basket_total()
-    local value = 0
-    for i in all(basket) do
-        value = value + i.cost
-    end
-    return value
-end
-
-function shopping_cursor_routine_update(options)
+    local total_items = #options
+    local current_cursor_pos = shopping.cursor.pos
 
     while shopping.is_open do
-        local cursor_item = options[shopping.cursor.pos]
+        local cursor_item = options[current_cursor_pos]
         if btnp(3) then -- down item
-            if shopping.cursor.pos < #options then
-                shopping.cursor.pos = shopping.cursor.pos + 1
+            if current_cursor_pos < total_items then
+                current_cursor_pos = current_cursor_pos + 1
             else
-                shopping.cursor.pos = #options + 1 -- is cancelAction
+                current_cursor_pos = total_items + 1 -- is cancelAction
             end
 
         end
         if btnp(2) then -- up item
-            if shopping.cursor.pos > 1 then
-                shopping.cursor.pos = shopping.cursor.pos - 1
+            if current_cursor_pos > 1 then
+                current_cursor_pos = current_cursor_pos - 1
             else
-                shopping.cursor.pos = #options + 1 -- is cancelAction
+                current_cursor_pos = total_items + 1 -- is cancelAction
             end
         end
         if btnp(0) then -- left decrease
-            if shopping.cursor.pos > #options then
-                shopping.cursor.pos =  #options + 1
+            if current_cursor_pos > total_items then
+                current_cursor_pos =  total_items + 1
             else
                 del(basket,cursor_item)
             end
@@ -76,30 +70,32 @@ function shopping_cursor_routine_update(options)
 
         end
         if btnp(1) then -- right increase
-            if shopping.cursor.pos > #options then
-                shopping.cursor.pos =  #options + 2
+            if current_cursor_pos > total_items then
+                current_cursor_pos =  total_items + 2
             else
-                if amount_in_basket(cursor_item.id) < cursor_item.available then
-                    add(basket,options[shopping.cursor.pos])
+                if shopping_amount_in_basket(cursor_item.id) < cursor_item.available then
+                    add(basket,options[current_cursor_pos])
                 end
             end
         end
-        if #options + 2 == shopping.cursor.pos and btnp(5) then
+        if total_items + 2 == current_cursor_pos and btnp(5) then
             shopping.is_open = false
         end
+        shopping.cursor.pos = current_cursor_pos
         yield()
     end
 end
 
-function shopping_routine_drawing(options)
+function shopping_routine_draw(options)
 
     shopping.is_open = true
     routines_add_new(
-            function() shopping_cursor_routine_update(options)  end,
+            function() shopping_routine_update(options)  end,
             ROUTINE_UPDATE,
             'shopping_update_id'
     )
     while shopping.is_open do
+        --TODO:create a init table function
         local box = {
             x = 20,
             y = 20,
@@ -127,10 +123,10 @@ function shopping_routine_drawing(options)
             print("$" .. item.cost, row_pos.x + 10 ,row_pos.y,text_color) -- 10 is 8 for sprite + 2 of margin
             print(item.name, row_pos.x + 24 ,row_pos.y,text_color)
 
-            print(amount_in_basket(item.id) .. "/" .. item.available, row_pos.x + 68 ,row_pos.y,text_color)
+            print(shopping_amount_in_basket(item.id) .. "/" .. item.available, row_pos.x + 68 ,row_pos.y,text_color)
             row_pos.y = row_pos.y + 8
         end
-        local total = "total: $" .. basket_total()
+        local total = "total: $" .. _shopping_basket_total()
         print(total,box_right_x - 6 - #total * letter_width,row_pos.y + 4,7)
 
         line(row_pos.x+2,box_bottom_y-10,box_right_x-4,box_bottom_y-10,7)
@@ -154,11 +150,19 @@ function shopping_routine_drawing(options)
 
 end
 
+function _shopping_basket_total()
+    local value = 0
+    for i in all(basket) do
+        value = value + i.cost
+    end
+    return value
+end
+
 --[[
 amount_in_basket
     return the number of a current item in the basket
 ]]
-function amount_in_basket(product_id)
+function shopping_amount_in_basket(product_id)
     return #tbl_filter(basket,function(b)
         return product_id == b.id
     end)
