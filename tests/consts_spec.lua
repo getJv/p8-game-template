009fb4c0eaf024ebf1037b6b89afefd17202b143
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-12-01 20:44:29 +0000

    chore(consts): tests clean up

--- tests/consts_spec.lua
+++ tests/consts_spec.lua
@@ -1,11 +1,5 @@
 package.path = package.path .. ";./?.lua;./vendor/?.lua;/app/vendor/?.lua"
-require('tests/pico_mocks')
-require('utils')
-require('consts')
 
 describe("consts.lua", function()
-    it("builds CONST from string with proper types", function()
-        assert.are.equal('4', CONST.letter_width)
-        assert.are.equal('8', CONST.cell_size)
-    end)
+
 end)
